c83aecab129e918ba8cbb229bcaef7f60acdfb26
Mod-Net CI <dev@mod-net.org>
2025-08-20 13:43:07 -0600

    updated flake

--- env-setup/flake.nix
+++ env-setup/flake.nix
@@ -1,9 +1,9 @@
 {
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    nixpkgs.follows = "rust-overlay/nixpkgs";
     flake-utils.url = "github:numtide/flake-utils";
     rust-overlay.url = "github:oxalica/rust-overlay";
+    rust-overlay.inputs.nixpkgs.follows = "nixpkgs";
   };
 
   outputs = { self, nixpkgs, flake-utils, rust-overlay, ... }: flake-utils.lib.eachDefaultSystem (system:
